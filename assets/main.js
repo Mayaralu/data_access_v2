d10215a9e93a3364874fa596a02c7408aa3bc0a5
Mayaralu <mayara.lu.98@gmail.com>
2019-03-17 21:49:12 -0300

    Adding the first functions

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,31 @@
 //This jQuery statement defines a self-invoking, anonymous function that runs after iframe.html has finished loading in the browser.
 // Initialise the Zendesk JavaScript API client
 // https://developer.zendesk.com/apps/docs/apps-v2
-var client = ZAFClient.init();
-client.invoke('resize', { width: '100%', height: '200px' });
+$(function() {
+  var client = ZAFClient.init();
+  client.invoke('resize', { width: '100%', height: '400px' });
+
+  showStart();
+  $("#get-tasks").click(function() {
+  getTaskData(client);
+
+});
+
+});
+
+function showStart() {
+  var source = $("#start-hdbs").html();
+  var template = Handlebars.compile(source);
+  var html = template();
+  $("#content").html(html);
+}
+
+function getTaskData(client) {
+  var settings = {
+    url: 'https://app.asana.com/api/1.0/projects/your_project_id/tasks',
+    headers: {"Authorization": "Bearer 0/d44bc72da8f853a0d5b0b0263e5dc186"},
+    type: 'GET',
+    dataType: 'json'
+  };
+  // make request
+}
