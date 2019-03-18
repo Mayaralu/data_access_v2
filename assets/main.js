8ed84d77b0417815c8f82da4bf9aa8bceb34f5c6
Mayaralu <mayara.lu.98@gmail.com>
2019-03-18 11:39:17 -0300

    Adição de Funções
    Adição de todas as funções necessárias para funcionamento.

--- assets/main.js
+++ assets/main.js
@@ -6,10 +6,10 @@ $(function() {
   client.invoke('resize', { width: '100%', height: '400px' });
 
   showStart();
-  $("#get-tasks").click(function() {
-  getTaskData(client);
 
-});
+  $("#get-tasks").click(function(){
+    getTaskData(client);
+  });
 
 });
 
@@ -21,11 +21,44 @@ function showStart() {
 }
 
 function getTaskData(client) {
+  // The settings object contains all the properties necessary to complete the API request
+  // based on the Asana's Get Project Tasks doc
   var settings = {
-    url: 'https://app.asana.com/api/1.0/projects/your_project_id/tasks',
+    url: 'https://app.asana.com/api/1.0/projects/1114672456098820/tasks',
     headers: {"Authorization": "Bearer 0/d44bc72da8f853a0d5b0b0263e5dc186"},
     type: 'GET',
     dataType: 'json'
   };
   // make request
+  client.request(settings).then(
+    function(data) { // If the request is successful, this callback function is executed
+      console.log(data);
+      showTaskData(data);
+    },
+    function(response) { // If not, this second one is executed. 
+      console.log(response);
+      showError(response);
+    }
+  );
+}
+
+function showTaskData(tasks) {
+  var context = {
+    project_tasks: tasks.data
+  };
+  var source = $("#tasks-hdbs").html();
+  var template = Handlebars.compile(source);
+  var html = template(context);
+  $("#content").html(html);
+}
+
+function showError(response) {
+  var context = {
+    'status': response.status,
+    'statusText': response.statusText
+  };
+  var source = $("#error-hdbs").html();
+  var template = Handlebars.compile(source);
+  var html = template(context);
+  $("#content").html(html);
 }
